9d811542da429e6385308959d3bf6b7c8def15fe
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:02:30 +0100

    fini

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
-  $age = NULL;
-  echo $age;
-  $age= 22;
-  echo $age;
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 1 Exercice 5</title>
+  </head>
+  <body>
+    <?php
+      $age = NULL;
+      echo $age;
+      $age= 22;
+      echo $age;
+     ?>
+  </body>
+</html>
